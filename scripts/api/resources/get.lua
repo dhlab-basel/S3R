6c9d2e4a3ff237e25f224c7b7297d63d67dea62b
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2018-09-26 12:08:59 +0200

    thumbnails and preview for a resource, renaming

--- scripts/api/resources/get.lua
+++ scripts/api/resources/get.lua
@@ -5,7 +5,20 @@ require "./model/resource"
 require "./model/parameter"
 require "./model/file"
 
-function getXMLContent(data)
+-- Constant
+thumbnail = {
+    small  = {height = 100, width =  75},
+    medium = {height = 140, width = 105},
+    large  = {height = 200, width = 150}
+}
+
+previewHeight = {
+    small  =  600,
+    medium = 1024,
+    large  = 1440
+}
+
+function getDublinCoreXML(data)
     local content = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" .. "\n" .. "<metadata>" .. "\n"
     local paramList = {
         "title",
@@ -112,7 +125,7 @@ function getResource()
     server.print(jsonstr)
 end
 
-function getFileResource()
+function getResourceContent()
     local id = string.match(uri, "%d+")
     local data = readRes(id)
 
@@ -168,7 +181,286 @@ function getFileResource()
     return
 end
 
-function getMetaDataFileResource()
+function getPreview()
+    local table1 = {}
+    local id = string.match(uri, "%d+")
+    local data = readRes(id)
+
+    -- Data does not exist in the database
+    if (data == nil) then
+        server.sendHeader('Content-type', 'application/json')
+        server.sendStatus(404)
+        print("no resource found in database")
+        return
+    end
+
+    local serverFilename = data["filename"]
+    local mimetype = data["mimetype"]
+
+    if (serverFilename == nil) or (mimetype == nil) then
+        server.sendHeader('Content-type', 'application/json')
+        server.sendStatus(500)
+        print("no file infos in data base found")
+        return
+    end
+
+    if (mimetype ~= "image/jpeg") and (mimetype ~= "image/tiff") and (mimetype ~= "image/jp2") and (mimetype ~= "image/png") then
+        print("it is not an image")
+        -- output of something else
+        return
+    end
+
+    local sipiImageSucces, img = SipiImage.new("files/" .. serverFilename)
+
+    if (not sipiImageSucces) then
+        print("fail")
+        return
+    end
+
+    local dimsBeforeSuccess, dimsBefore = img.dims(img)
+
+    if (not dimsBeforeSuccess) then
+        print("fail")
+        return
+    end
+
+    if (dimsBefore.ny < previewHeight.small) then
+        table1["resolution"] = {height=dimsBefore.ny, width=dimsBefore.nx}
+    else
+        img.scale(img, "," .. previewHeight.small)
+
+        local dimsAfterSuccess, dimsAfter = img.dims(img)
+
+        if (not dimsAfterSuccess) then
+            print("fail")
+            return
+        end
+
+        table1["resolution"] = {height=dimsAfter.ny, width=dimsAfter.nx}
+    end
+
+    table1["mimetype"] = mimetype
+
+    server.setBuffer()
+
+    local success, jsonstr = server.table_to_json(table1)
+    if not success then
+        server.sendStatus(500)
+        server.log(jsonstr, server.loglevel.err)
+        return false
+    end
+
+    server.sendHeader('Content-type', 'application/json')
+    server.sendStatus(200)
+    server.print(jsonstr)
+end
+
+function getPreviewContent()
+    local id = string.match(uri, "%d+")
+    local data = readRes(id)
+
+    -- Data does not exist in the database
+    if (data == nil) then
+        server.sendHeader('Content-type', 'application/json')
+        server.sendStatus(404)
+        print("no resource found in database")
+        return
+    end
+
+    local serverFilename = data["filename"]
+    local mimetype = data["mimetype"]
+
+    if (serverFilename == nil) or (mimetype == nil) then
+        server.sendHeader('Content-type', 'application/json')
+        server.sendStatus(500)
+        print("no file infos in data base found")
+        return
+    end
+
+    if (mimetype ~= "image/jpeg") and (mimetype ~= "image/tiff") and (mimetype ~= "image/jp2") and (mimetype ~= "image/png") then
+        print("it is not an image")
+
+        -- send the not image
+        local fileContent, errMsg = readFile(serverFilename)
+
+        if (errMsg ~= nil) then
+            server.sendHeader('Content-type', 'application/json')
+            server.sendStatus(errMsg)
+            print("file or path does not exist")
+            return
+        end
+
+        if (fileContent == nil) then
+            server.sendHeader('Content-type', 'application/json')
+            server.sendStatus(500)
+            print("failed to read file")
+            return
+        end
+
+        server.setBuffer()
+        server.sendHeader('Access-Control-Expose-Headers','Content-Disposition');
+        server.sendHeader('Content-type', mimetype)
+        server.sendStatus(200)
+        server.print(fileContent)
+        return
+    end
+
+    local succes, img = SipiImage.new("files/" .. serverFilename)
+
+    if (not succes) then
+        print("fail")
+        return
+    end
+
+    img.scale(img, "," .. previewHeight.small)
+
+    local dimsSuccess, dims = img.dims(img)
+
+    if (not dimsSuccess) then
+        print("fail")
+        return
+    end
+
+    server.sendHeader('Content-type', 'image/jpeg')
+    local writeSuccess, errorMsg = img.write(img, "HTTP.jpg")
+
+end
+
+function getThumbnailSize()
+    local table1 = {}
+
+    local size
+    local startSize, endSize = string.find(uri, "thumbnail/")
+    if (startSize ~= nil) and (endSize ~= nil) then
+        size = string.sub(uri, endSize+1, #uri)
+    end
+
+    -- Checks if thumbnail size is valid
+    if (thumbnail[size] == nil) then
+        print("bad thumbnail size")
+        server.sendStatus(400)
+        return
+    end
+
+    local id = string.match(uri, "%d+")
+    local data = readRes(id)
+
+    -- Data does not exist in the database
+    if (data == nil) then
+        server.sendHeader('Content-type', 'application/json')
+        server.sendStatus(404)
+        print("no resource found in database")
+        return
+    end
+
+    local serverFilename = data["filename"]
+
+    if (serverFilename == nil) then
+        server.sendHeader('Content-type', 'application/json')
+        server.sendStatus(500)
+        print("no file infos in data base found")
+        return
+    end
+
+    table1["resolution"] = {height=thumbnail[size].height, width=thumbnail[size].width }
+
+    server.setBuffer()
+
+    local success, jsonstr = server.table_to_json(table1)
+    if not success then
+        server.sendStatus(500)
+        server.log(jsonstr, server.loglevel.err)
+        return false
+    end
+
+    server.sendHeader('Content-type', 'application/json')
+    server.sendStatus(200)
+    server.print(jsonstr)
+end
+
+function getThumbnailSizeContent()
+    local size
+    local startSize, endSize = string.find(uri, "thumbnail/")
+    local startContent, endContent = string.find(uri, "/content")
+    if (startSize ~= nil) and (endSize ~= nil) and (startContent ~= nil) and (endContent ~= nil) then
+        size = string.sub(uri, endSize+1, startContent-1)
+    end
+
+    -- Checks if thumbnail size is valid
+    if (thumbnail[size] == nil) then
+        print("bad thumbnail size")
+        server.sendStatus(400)
+        return
+    end
+
+    local id = string.match(uri, "%d+")
+    local data = readRes(id)
+
+    -- Data does not exist in the database
+    if (data == nil) then
+        server.sendHeader('Content-type', 'application/json')
+        server.sendStatus(404)
+        print("no resource found in database")
+        return
+    end
+
+    local serverFilename = data["filename"]
+    local mimetype = data["mimetype"]
+
+    if (serverFilename == nil) or (mimetype == nil) then
+        server.sendHeader('Content-type', 'application/json')
+        server.sendStatus(500)
+        print("no file infos in data base found")
+        return
+    end
+
+    if (mimetype ~= "image/jpeg") and (mimetype ~= "image/tiff") and (mimetype ~= "image/jp2") and (mimetype ~= "image/png") then
+        print("not a image")
+        return
+    end
+
+    local succes, img = SipiImage.new("files/" .. serverFilename)
+
+    if (not succes) then
+        print("fail")
+        return
+    end
+
+    local dimsSuccess, dims = img.dims(img)
+
+    if (not dimsSuccess) then
+        print("fail")
+        return
+    end
+
+    local imageRatio = dims.nx / dims.ny
+    local thumbnailRatio = thumbnail[size].width / thumbnail[size].height
+
+    if (imageRatio < thumbnailRatio) then
+        print("höher als Breite")
+        img.scale(img, thumbnail[size].width .. ",")
+        local success, dims = img.dims(img)
+        if success then
+            local startY = (dims.ny -thumbnail[size].height) / 2
+            img.crop(img, "0," .. startY .. "," .. thumbnail[size].width .. "," .. thumbnail[size].height)
+        end
+    elseif (imageRatio > thumbnailRatio) then
+        print("breiter als Höhe")
+        img.scale(img, "," .. thumbnail[size].height)
+        local success, dims = img.dims(img)
+        if success then
+            local startX = (dims.nx - thumbnail[size].width) / 2
+            img.crop(img, startX .. ",0," .. thumbnail[size].width .. "," .. thumbnail[size].height)
+        end
+    else
+        img.scale(img, thumbnail[size].width .. "," .. thumbnail[size].height)
+    end
+
+    server.sendHeader('Content-type', 'image/jpeg')
+    local writeSuccess, errorMsg = img.write(img, "HTTP.jpg")
+end
+
+function getMetaDataXML()
     local id = string.match(uri, "%d+")
     local data = readRes(id)
 
@@ -191,7 +483,7 @@ function getMetaDataFileResource()
         end
     end
 
-    local fileContent = getXMLContent(data)
+    local fileContent = getDublinCoreXML(data)
 
     server.setBuffer()
     server.sendHeader('Access-Control-Expose-Headers','Content-Disposition');
@@ -209,8 +501,12 @@ uri = server.uri
 routes = {}
 routes[baseURL .. "/resources$"] = getResources
 routes[baseURL .. "/resources/%d+$"] = getResource
-routes[baseURL .. "/resources/%d+/file$"] = getFileResource
-routes[baseURL .. "/resources/%d+/metadata"] = getMetaDataFileResource
+routes[baseURL .. "/resources/%d+/content$"] = getResourceContent
+routes[baseURL .. "/resources/%d+/preview$"] = getPreview
+routes[baseURL .. "/resources/%d+/preview/content$"] = getPreviewContent
+routes[baseURL .. "/resources/%d+/thumbnail/%a+$"] = getThumbnailSize
+routes[baseURL .. "/resources/%d+/thumbnail/%a+/content$"] = getThumbnailSizeContent
+routes[baseURL .. "/resources/%d+/metadata/xml"] = getMetaDataXML
 
 for route, func in pairs(routes) do
     if (string.match(uri, route) ~= nil) then
