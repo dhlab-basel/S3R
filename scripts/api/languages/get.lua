b1c529234def38b227be7c7342103f99a07ff73f
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2018-10-10 16:35:37 +0200

    adding language scripts

---
+++ scripts/api/languages/get.lua
@@ -0,0 +1,70 @@
+print("---- GET language script ----")
+
+function getLanguages()
+    local table1 = {}
+    table1["data"] = readAllLang({})
+
+    if (#table1["data"] > 0) then
+        table1["status"] = "successful"
+    else
+        table1["status"] = "no data were found"
+        table1["data"] = {{}}
+    end
+
+    server.setBuffer()
+
+    local success, jsonstr = server.table_to_json(table1)
+    if not success then
+        server.sendStatus(500)
+        server.log(jsonstr, server.loglevel.err)
+        return false
+    end
+
+    server.sendHeader('Content-type', 'application/json')
+    server.sendStatus(200)
+    server.print(jsonstr)
+end
+
+function getLanguage()
+    local table1 = {}
+    local id = string.match(uri, "%d+")
+    table1["data"] = readLang(id)
+
+    if (table1["data"] ~= nil) then
+        table1["status"] = "successful"
+    else
+        table1["status"] = "no data were found"
+        table1["data"] = {{}}
+    end
+
+    server.setBuffer()
+
+    local success, jsonstr = server.table_to_json(table1)
+    if not success then
+        server.sendStatus(500)
+        server.log(jsonstr, server.loglevel.err)
+        return false
+    end
+
+    server.sendHeader('Content-type', 'application/json')
+    server.sendStatus(200)
+    server.print(jsonstr)
+end
+
+-- Checking of the url and appling the appropriate function
+baseURL = "^/api"
+uri = server.uri
+
+routes = {}
+routes[baseURL .. "/languages$"] = getLanguages
+routes[baseURL .. "/languages/%d+$"] = getLanguage
+
+for route, func in pairs(routes) do
+    if (string.match(uri, route) ~= nil) then
+        func()
+        return
+    end
+end
+
+-- Fails if no url matches
+server.sendStatus(404)
