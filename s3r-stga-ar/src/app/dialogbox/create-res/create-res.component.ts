d6cfe7cf3382c0577e31b0e00325ea5bb3cb21c9
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2018-11-10 21:43:13 +0100

    create res validation

--- s3r-stga-ar/src/app/dialogbox/create-res/create-res.component.ts
+++ s3r-stga-ar/src/app/dialogbox/create-res/create-res.component.ts
@@ -2,6 +2,7 @@ import {Component, Inject, OnInit} from "@angular/core";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
 import {ApiService} from "../../services/api.service";
 import {File} from "../../model/file";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
     selector: "app-create-res",
@@ -12,25 +13,8 @@ export class CreateResComponent implements OnInit {
     selectedFile = null;
     selectedFileSize: string;
     fileTypeList: string[];
-
     data: string;
-
-    // Dublin Core Fields
-    title: string;
-    creator: string;
-    subject: string;
-    description: string;
-    publisher: string;
-    contributor: string;
-    date_start: string;
-    date_end: string;
-    format: string;
-    identifier: string;
-    source: string;
-    language: string;
-    relation: string;
-    coverage: string;
-    rights: string;
+    form: FormGroup;
 
     constructor(private dialogRef: MatDialogRef<CreateResComponent>, @Inject(MAT_DIALOG_DATA) data,
                 private apiService: ApiService,
@@ -40,6 +24,26 @@ export class CreateResComponent implements OnInit {
     }
 
     ngOnInit() {
+        this.form = new FormGroup({
+            file: new FormControl(),
+            title: new FormControl("", [Validators.required, Validators.pattern(/^\w+/)]),
+            creator: new FormControl("", []),
+            subject: new FormControl("", []),
+            description: new FormControl("", []),
+            publisher: new FormControl("", []),
+            contributor: new FormControl("", []),
+            dateStart: new FormControl("", [Validators.required, Validators.min(1000), Validators.max(9999)]),
+            dateEnd: new FormControl("", [Validators.required, Validators.min(1000), Validators.max(9999)]),
+            format: new FormControl("", [Validators.required]),
+            identifier: new FormControl("", [Validators.required]),
+            language: new FormControl("", []),
+            rights: new FormControl("", [])
+
+            // Dublin Core Fields which are not used
+            // source: new FormControl("", []),
+            // relation: new FormControl("", []),
+            // coverage: new FormControl("", []),
+        });
     }
 
     create() {
@@ -47,45 +51,28 @@ export class CreateResComponent implements OnInit {
             return;
         }
 
-        if ((this.title == null) || (this.title === "")) {
-            console.log("title missing");
-            return;
-        }
-
-        if ((this.identifier == null) || (this.identifier === "")) {
-            console.log("identifier missing");
-            return;
-        }
-
-        if (!/^\d{4}$/.test(this.date_start)) {
-            console.log("date_start is invalid");
-            return;
-        }
-
-        if (!/^\d{4}$/.test(this.date_end)) {
-            console.log("date_end is invalid");
-            return;
-        }
-
         const fd = new FormData();
         fd.append("image", this.selectedFile, this.selectedFile.name);
         fd.append("dump", "");
-        fd.append("title", this.title);
-        fd.append("creator", this.creator);
-        fd.append("subject", this.subject);
-        fd.append("description", this.description);
-        fd.append("publisher", this.publisher);
-        fd.append("contributor", this.contributor);
-        fd.append("date_start", this.date_start);
-        fd.append("date_end", this.date_end);
-        fd.append("format", this.file.simpleFormTomimeType(this.format));
-        fd.append("identifier", this.identifier);
-        fd.append("source", this.source);
-        fd.append("language", this.language);
-        fd.append("relation", this.relation);
-        fd.append("coverage", this.coverage);
-        fd.append("rights", this.rights);
+        fd.append("title", this.form.get("title").value);
+        fd.append("creator", this.form.get("creator").value);
+        fd.append("subject", this.form.get("subject").value);
+        fd.append("description", this.form.get("description").value);
+        fd.append("publisher", this.form.get("publisher").value);
+        fd.append("contributor", this.form.get("contributor").value);
+        fd.append("date_start", this.form.get("dateStart").value);
+        fd.append("date_end", this.form.get("dateEnd").value);
+        fd.append("format", this.file.simpleFormTomimeType(this.form.get("format").value));
+        fd.append("identifier", this.form.get("identifier").value);
+        fd.append("language", this.form.get("language").value);
+        fd.append("rights", this.form.get("rights").value);
         fd.append("collection_id", this.data["colID"]);
+
+        // Dublin Core Fields which are not used
+        // fd.append("source", this.form.get("source").value);
+        // fd.append("relation", this.form.get("relation").value);
+        // fd.append("coverage", this.form.get("coverage").value);
+
         this.apiService.createResource(fd)
             .subscribe((result) => {
                 console.log(result);
@@ -107,4 +94,36 @@ export class CreateResComponent implements OnInit {
             console.log("Wrong mime type");
         }
     }
+
+    getErrorTitle(): string {
+        return this.form.get("title").hasError("required") ? "Titel muss eingegeben werden":
+            this.form.get("title").hasError("pattern") ? "Ungültiger Titel":
+                "";
+    }
+
+    getErrorDateStart(): string {
+        return this.form.get("dateStart").hasError("required") ? "Ungültiges Anfangsjahr":
+            this.form.get("dateStart").hasError("min") ? "Mindestjahr ist 1000":
+                this.form.get("dateStart").hasError("max") ? "Ungültiges Anfangsjahr":
+                    "";
+    }
+
+    getErrorDateEnd(): string {
+        return this.form.get("dateEnd").hasError("required") ? "Ungültiges Endjahr":
+                this.form.get("dateEnd").hasError("min") ? "Mindestjahr ist 1000":
+                    this.form.get("dateEnd").hasError("max") ? "Ungültiges Endjahr":
+                        "";
+    }
+
+    getErrorFormat(): string {
+        return this.form.get("format").hasError("required") ? "Bitte Format auswählen":
+                "";
+    }
+
+    getErrorIdentifier(): string {
+        return this.form.get("identifier").hasError("required") ? "Signatur muss eingegeben werden":
+                "";
+    }
+
+
 }
